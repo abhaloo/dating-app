fb075a43c5a5fed6e196cdce12fd8566b6480b4d
Adeel Bhaloo <abhaloo@hotmail.com>
2021-10-22 17:09:48 -0600

    created user entity

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        //User ID
+        public int Id { get; set; }
+        
+        //user name
+        public int Username { get; set; }
+    
+    }
+}
